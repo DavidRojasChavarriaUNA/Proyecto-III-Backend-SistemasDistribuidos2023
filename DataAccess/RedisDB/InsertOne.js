90507e0104cc5ab6ff36e7beefa332a0c959fa15
David Rojas Chavarria <davidmfrojas@gmail.com>
2023-11-20 00:41:34 -0600

    Backend
    Backend

--- DataAccess/RedisDB/InsertOne.js
+++ DataAccess/RedisDB/InsertOne.js
@@ -5,7 +5,8 @@ const {
 } = require('./ClientDB');
 
 exports.InsertOne = async (keybase, data) => {
-    data._id = new Date().getTime();
+    if(data._id === 0)
+        data._id = new Date().getTime();
     const dataText = JSON.stringify(data);
     const result = await clientRedisBD.set(`${keybase}${data._id}`, dataText);
     if(result !== 'OK')
